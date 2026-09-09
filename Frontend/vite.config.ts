d386957edfed67ba7f5f84d392a841ea646fe748
avimo91 <avimo64@gmail.com>
2026-09-09 12:00:23 +0300

    Initial commit

---
+++ Frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config' // Instead "vite" to use testing
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        host: "0.0.0.0",
+        port: 5173
+    },
+    test: {
+        environment: "jsdom",
+    }
+})
